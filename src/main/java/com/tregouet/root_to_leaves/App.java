62cd973252c78bf235b7ffc22120d38895bebd6f
Tregouet <gael.tregouet@gmail.com>
2021-06-18 15:50:55 +0200

    Add files via upload

---
+++ src/main/java/com/tregouet/root_to_leaves/App.java
@@ -0,0 +1,13 @@
+package com.tregouet.root_to_leaves;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
